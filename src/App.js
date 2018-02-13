285b09253ed1fc587dc29d849db7d9a655acc118
thelongmarch <themarch@163.com>
2018-02-13 22:31:59 +0800

    modified

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import Header from './components/Header';
 import Home from './components/Home';
 
 class App extends Component {
+
     render() {
+        let content = "";
+        if(true){
+            content = "hello!";
+        }
+        const user = {
+            name:'jordon',
+            hobbies:["sports","reading"]
+        }
+
         return (
             <div className='container'>
                 <div className='row'>
@@ -13,12 +23,17 @@ class App extends Component {
                 </div>
                 <div className='row'>
                     <div className='col-xs-1'>
-                        <h1>hello!</h1>
+                        <h1>{1+1}</h1>
+                        <h1>{'字符串aaa'}</h1>
+                        <h1>{content}</h1>
+                        <h1>{true? "world1":"world2"}</h1>
                     </div>
                 </div>
                 <div className='row'>
                     <div className='col-xs-1'>
-                        <Home />
+                        <Home  name={"name1"} age={12} user={user}>
+                            <p>i am child</p>
+                        </Home>
                     </div>
                 </div>
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+
   render() {
     return (
         <div className='container'>
             <div className='row'>
                 <div className='col-xs-1'>
-                    <h1>home</h1>
+                    <div>your name is {this.props.name},your age is {this.props.age}</div>
+                    <div>
+                        <h4>hobbies</h4>
+                        <ul>
+                            {this.props.user.hobbies.map((v,k)=><li key={k}>{v}</li>)}
+                        </ul>
+                    </div>
+
+                    <div>{this.props.children}</div>
+                    <button className="btn btn-primary" >make me older</button>
+
                 </div>
             </div>
         </div>
@@ -14,4 +26,11 @@ export default class Home extends Component {
   }
 }
 
+Home.propTypes ={
+    name:PropTypes.string,
+    age:PropTypes.number,
+    user:PropTypes.object,
+    children:PropTypes.element.isRequired
+
+}
 
